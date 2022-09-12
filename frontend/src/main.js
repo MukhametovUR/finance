9afd40a8866589b13d9e9c1b34144a86c4bf36cd
MukhametovUR <ural-lan4x4@yandex.ru>
2022-09-12 09:04:13 +0500

    add axios in frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+import Axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.prototype.$http = Axios
+const token = localStorage.getItem('token')
+if(token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+}
 
 createApp(App).use(store).use(router).mount('#app')
