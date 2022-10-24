e2e45352769383c001730fe01d97dd7f41c8e3f4
MukhametovUR <ural-lan4x4@yandex.ru>
2022-10-24 23:00:59 +0500

    decomposition input

--- frontend/src/components/TextInput.vue
+++ frontend/src/components/TextInput.vue
@@ -1,11 +1,28 @@
 <template>
-
+  <input  type="type"
+          name="name"
+          :value="modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
+          :placeholder="placeholder">
 </template>
 
 <script>
-
+export default {
+  name: 'TextInput',
+  emits: ['update:modelValue'],
+  props: {
+    'modelValue': {
+      type: [String, Number]
+    },
+    'placeholder': String,
+    'type': {
+      type: String,
+      default: 'text'
+    },
+  }
+}
 </script>
 
 <style>
-
+@import "@/assets/styles/styles.scss";
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import axios from 'axios'
 import './assets/styles/styles.scss'
 
+const app = createApp(App)
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 axios.interceptors.response.use(
@@ -23,7 +24,15 @@ axios.interceptors.response.use(
   }
 );
 
-createApp(App).config.productionTip = false
+// install custom inputs
+import inputs from '@/components'
+
+let components = [...inputs]
+components.forEach(component => {
+  app.component(component.name, component)
+})
+
+app.config.productionTip = false
 
 
-createApp(App).use(store).use(router).mount('#app')
+app.use(store).use(router).mount('#app')
